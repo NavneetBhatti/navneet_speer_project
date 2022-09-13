f670b2a23e183b4269df33c090f462fef8c715a3
NavneetKaurBhatti <navneetkaur81960@gmail.com>
2022-09-13 13:02:23 -0400

    testing branch

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import  Info from './Info.jsx';
 import ActivityDetail from './ActivityDetail.jsx';
 
 //function..
+//testing branch
 function fetchActivities() {
 	return fetch('https://aircall-job.herokuapp.com/activities')
 		.then(response => {
